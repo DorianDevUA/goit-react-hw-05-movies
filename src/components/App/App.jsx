5860535b4dd253640e24e54dd7e121c0ab554298
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-24 20:17:37 +0400

    add components to Route for rendering

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
-// import { useEffect, useState } from 'react';
-// import API from '../../services/themoviedb-api';
-
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from '../SharedLayout';
 import HomePage from '../../pages/HomePage';
+import MoviesPage from '../../pages/MoviesPage';
+import MovieDetailsPage from '../../pages/MovieDetailsPage';
+import Cast from '../Cast';
+import Reviews from '../Reviews';
 
 const App = () => {
   return (
@@ -11,10 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="movies" element={<div>MoviesPage</div>} />
-          <Route path="movies/:movieId" element={<div>MovieDetailsPage</div>}>
-            <Route path="cast" element={<div>Cast component</div>} />
-            <Route path="reviews" element={<div>Reviews component</div>} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
