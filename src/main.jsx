dac390f88717b003a7271a57d159639dabd9c0f4
Dorian Devua <Dorian.DevUa@gmail.com>
2024-06-25 21:12:17 +0400

    add GlobalStyle component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize';
+import { GlobalStyle } from './styles/GlobalStyle';
 import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies">
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
